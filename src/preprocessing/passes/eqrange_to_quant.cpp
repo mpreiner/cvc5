494c5b7f1b825ffb0fbeedc40ade7b6e428bc977
Haniel Babosa <hanielbbarbosa@gmail.com>
2018-08-08 18:31:49 -0500

    transient

---
+++ src/preprocessing/passes/eqrange_to_quant.cpp
@@ -0,0 +1,86 @@
+/*********************                                                        */
+/*! \file real_to_int.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Haniel Barbosa
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2018 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief The EqrangeToQuant preprocessing pass
+ **
+ ** Converts the eqrange predicate into its axiomatization
+ **/
+
+#include "preprocessing/passes/eqrange_to_quant.h"
+
+#include <string>
+
+#include "theory/rewriter.h"
+
+namespace CVC4 {
+namespace preprocessing {
+namespace passes {
+
+EqrangeToQuant::EqrangeToQuant(PreprocessingPassContext* preprocContext)
+    : PreprocessingPass(preprocContext, "eqrange-to-quant"){};
+
+Node EqrangeToQuant::eqrangeToQuantInternal(TNode n,
+                                            NodeMap& cache)
+{
+  Trace("eqrange-as-quant-debug") << "Convert : " << n << "\n";
+  NodeMap::iterator find = cache.find(n);
+  if (find != cache.end())
+  {
+    return (*find).second;
+  }
+  Node ret = n;
+  NodeManager* nm = NodeManager::currentNM();
+  Kind k = n.getKind();
+  if (k == kind::EQRANGE)
+  {
+    // bounds are of bitvector type
+    Assert(n[2].getType().isBitVector() && n[3].getType().isBitVector());
+    // mk bound var list
+    Node index = nm->mkBoundVar(n[2].getType());
+    Node boundVarList = nm->mkNode(kind::BOUND_VAR_LIST, std::vector<Node>(index));
+    // mk quantified formula
+    // forall i. i < lb v i > ub v a[i] != b[i]
+
+  }
+  bool childChanged = false;
+  std::vector<Node> children;
+  for (unsigned i = 0, size = n.getNumChildren(); i < size; ++i)
+  {
+    Node nc = eqrangeToQuantInternal(n[i], cache, var_eq);
+    childChanged = childChanged || nc != n[i];
+    children.push_back(nc);
+  }
+  if (childChanged)
+  {
+    ret = nm->mkNode(k, children);
+  }
+
+  Trace("eqrange-as-quant-debug") << "Converted : " << ret << "\n";
+
+  cache[n] = ret;
+  return ret;
+}
+
+PreprocessingPassResult EqrangeToQuant::applyInternal(
+    AssertionPipeline* assertionsToPreprocess)
+{
+  unordered_map<Node, Node, NodeHashFunction> cache;
+  for (unsigned i = 0, size = assertionsToPreprocess->size(); i < size; ++i)
+  {
+    assertionsToPreprocess->replace(
+        i, eqrangeToQuantInternal((*assertionsToPreprocess)[i], cache));
+  }
+  return PreprocessingPassResult::NO_CONFLICT;
+}
+
+}  // namespace passes
+}  // namespace preprocessing
+}  // namespace CVC4
