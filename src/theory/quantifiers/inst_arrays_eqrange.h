010b4ff75cd6500742af5c239093b36c934c6084
Haniel Barbosa <hanielbbarbosa@gmail.com>
2018-08-10 19:52:14 -0500

    doc

--- src/theory/quantifiers/inst_arrays_eqrange.h
+++ src/theory/quantifiers/inst_arrays_eqrange.h
@@ -25,21 +25,39 @@ namespace CVC4 {
 namespace theory {
 namespace quantifiers {
 
-/** Instantiation for arrays eqrange
+/** Instantiation strategy for arrays equality range
+ *
+ * For quantifiers representing arrays equality within a range, this module
+ * determines if there is an index within the range on which the respective
+ * arrays are different, introducing instantiations is such a case. If no
+ * instance can be derived, then the quantifier is true in the current model.
  */
 class InstArraysEqrange : public QuantifiersModule
 {
  public:
   InstArraysEqrange(QuantifiersEngine* qe);
   ~InstArraysEqrange() {}
-  /** Needs check. */
+  /** Lets quantifier engine knows this module needs a last effort check. */
   bool needsCheck(Theory::Effort e) override;
+  /** Lets quantifier engine knows this module needs a model */
   QEffort needsModel(Theory::Effort e) override;
-  void checkOwnership(Node q) override;
-  /** Check.
+  /** Determines whether a given quantifier is an eqrange quantifier
    *
+   * Eqrange quantifiers are claimed, with this module taking full ownership
+   */
+  void checkOwnership(Node q) override;
+  /** Check
+   *
+   * Determines whether, for each currently asserted claimed quantifier, there
+   * exists an index within the respective quantifier's bounds on which its
+   * arrays differ
+   *
+   * The check is based on the model values of the bounds, arrays and indices.
+   *
+   * An instance is added with the respective index when the arrays differ
    */
   void check(Theory::Effort e, QEffort quant_e) override;
+  /** Returns true for claimed quantifiers */
   bool checkCompleteFor(Node q) override;
   /** Identify. */
   std::string identify() const override
@@ -50,7 +68,9 @@ class InstArraysEqrange : public QuantifiersModule
  private:
   /** whether q = forall x : (bv sz). (or [(x > lb) (x < ub)] (= a[x] b[x]))) */
   bool isEqrangeQuant(Node q);
+  /** set of claimed quantifiers */
   std::unordered_set<Node, NodeHashFunction> d_claimed_quants;
+  /** Bounds and arrays associated with each eqrange quantifier  */
   struct QuantInfo
   {
     /** lower bound */
@@ -61,6 +81,7 @@ class InstArraysEqrange : public QuantifiersModule
     Node a1;
     Node a2;
   };
+  /** map from claimed quantifiers to bound/array information */
   std::map<Node, QuantInfo> d_quant_to_info;
 }; /* class InstArraysEqrange */
 
