582834e3ecb50ca907c23b48376db4466b494d0a
mpreiner <mpreiner@users.noreply.github.com>
2020-09-25 22:10:59 +0000

    Update copyright headers for modified files.

--- src/test.cpp
+++ src/test.cpp
@@ -1,1 +1,18 @@
+/*********************                                                        */
+/*! \file test.cpp
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   
+ ** This file is part of the CVC4 project.
+ ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
+ ** (in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file COPYING in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief [[ Add one-line brief description here ]]
+ **
+ ** [[ Add lengthier description here ]]
+ ** \todo document this file
+ **/
+
 missing header
