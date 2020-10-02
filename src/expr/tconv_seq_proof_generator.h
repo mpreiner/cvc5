0cc73e23dc44bf756cedcc5f81f4b69ebc657991
mpreiner <mpreiner@users.noreply.github.com>
2020-10-02 03:31:03 +0000

    Update copyright headers for modified files.

--- src/expr/tconv_seq_proof_generator.h
+++ src/expr/tconv_seq_proof_generator.h
@@ -2,7 +2,7 @@
 /*! \file tconv_seq_proof_generator.h
  ** \verbatim
  ** Top contributors (to current version):
- **   Andrew Reynolds, Mathias Preiner, Alex Ozdemir
+ **   Andrew Reynolds, mpreiner, Mathias Preiner
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory and their institutional affiliations.
--- src/theory/trust_node.cpp
+++ src/theory/trust_node.cpp
@@ -2,7 +2,7 @@
 /*! \file trust_node.cpp
  ** \verbatim
  ** Top contributors (to current version):
- **   Andrew Reynolds, Mathias Preiner, Alex Ozdemir
+ **   Andrew Reynolds, mpreiner, Mathias Preiner
  ** This file is part of the CVC4 project.
  ** Copyright (c) 2009-2020 by the authors listed in the file AUTHORS
  ** in the top-level source directory and their institutional affiliations.
